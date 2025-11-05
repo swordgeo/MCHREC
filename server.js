be984f2ead7469b3e6bd1e4b6f69dda2c366458d
swordgeo <swordgeo1094@yahoo.com>
2025-11-05 17:24:15 -0500

    Civil War pack and Linktree/BuyMeACoffee links

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ app.listen(3000, function() {
 
 // updatePackData(pool);
 
-// updateCardData(pool, "silk"); 
+// updateCardData(pool, "cw");
 
 // also will have to manually insert new heroes into hero_names.json (probably the only JSON we're keeping)
 // before running this file and separately running json/photo.py
